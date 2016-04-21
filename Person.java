099608c6bf871f4a0220119644651f84a54ec5d7
Charlesexum1 <cexum@uga.edu>
2016-04-21 02:46:57 -0400

    Update Person.java

--- Person.java
+++ Person.java
@@ -22,12 +22,35 @@ public class Person {
 	public String getName() { // "" ""
 		return name;
 		}	
+	public boolean comparePeople(Person otherPerson){
+		return (this.name.equals(otherPerson.name)) &&
+			   (this.age == otherPerson.age);
+		}	
+	public boolean compareNames(Person otherPerson){
+		return (this.name.equals(otherPerson.name));
+		}
+	public boolean compareAge(Person otherPerson){
+		return (this.age == otherPerson.age);
+		}
+	public boolean isOlder(Person otherPerson){
+		return (this.age > otherPerson.age);
+		}
+	public boolean isYounger(Person otherPerson){
+		return (this.age < otherPerson.age);
+		}							
 		
 	
 	public static void main (String args[]) {
 		Person Graf = new Person();
 		Graf.setInfo("Charles DeGrapharee Exum", 26);
 		System.out.println(Graf.getAge() + " " + Graf.getName());
+		Person alsoGraf = new Person();
+		alsoGraf.setInfo("Graf", 26);
+		System.out.println(alsoGraf.comparePeople(Graf));
+		System.out.println(alsoGraf.compareNames(Graf));
+		System.out.println(alsoGraf.compareAge(Graf));
+		System.out.println(alsoGraf.isOlder(Graf));
+		System.out.println(alsoGraf.isYounger(Graf));
 		
 	}
 }
